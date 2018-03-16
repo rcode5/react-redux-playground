2856ff0e0e08b95609dd68c4b80b8526fdfe744f
Mr Rogers <jon@carbonfive.com>
2018-03-16 08:43:43 -0700

    styled more stuffs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 import { connect } from 'react-redux';
+import classnames from "classnames";
 import { INCREMENT, DECREMENT, UPDATE_STORE} from "./actions";
 import _ from "lodash";
+
+function ppJson(obj) { return JSON.stringify(obj, null, 2); }
 
 class Container extends Component {
 
@@ -27,13 +30,15 @@ class Container extends Component {
     const renderInputs = this.renderInputs.bind(this);
     return (
         <div className={styles.container}>
+        <div className={styles.flex_row}>
+        <div className={ classnames(styles.wrapper, styles.container_wrapper) }>
         <h2>Container</h2>
-        <pre><code>{JSON.stringify(this.props)}</code></pre>
-        <form>
-        { renderInputs() }
-        </form>
-        <Sub1 {...this.props}/>
-        <Sub2 />
+        <pre><code>{ppJson(this.props)}</code></pre>
+        <form>{ renderInputs() }</form>
+        </div>
+        <ConnectedSub1 onChange={this.props.onChange}/>
+        <Sub2  {...this.props} />
+        </div>
         </div>
     );
   }
@@ -41,15 +46,24 @@ class Container extends Component {
 
 Container.defaultProps = {
   a: "1",
-  b: "2"
+  b: "2",
+  c: "3",
 };
 
 class Sub1 extends Component {
   render() {
     return (
         <div className={styles.sub1}>
+        <div className={styles.wrapper}>
         <h3>sub1</h3>
-        <pre><code>{JSON.stringify(this.props)}</code></pre>
+        <pre><code>{ppJson(this.props)}</code></pre>
+        <div className="input-group">
+        <label>a</label>
+        <input type="text" value={this.props.a} onChange={
+          (e) => this.props.onChange({ a: e.target.value })
+        } />
+        </div>
+        </div>
         </div>
     );
   }
@@ -59,8 +73,16 @@ class Sub2 extends Component {
   render() {
     return (
         <div className={styles.sub2}>
+        <div className={styles.wrapper}>
         <h3>sub2</h3>
-        <pre><code>{JSON.stringify(this.props)}</code></pre>
+        <pre><code>{ppJson(this.props)}</code></pre>
+        <div className="input-group">
+        <label>b</label>
+        <input type="text" value={this.props.a} onChange={
+          (e) => this.props.onChange({ b: e.target.value })
+        } />
+        </div>
+        </div>
         </div>
     );
   }
@@ -81,9 +103,12 @@ class DisconnectedCounter extends Component {
     console.log("inner props", this.props);
     return(
         <div className={styles.app} id={ `counter${id}` } >
-        <span>ID: {this.props.id} {JSON.stringify(this.props)}</span>
-        <p>{ this.props.count }</p>
+        <pre><code>{ppJson(this.props)}</code></pre>
+        <p>
         <button onClick={() => this.props.increment(id)}>Increment</button>
+        <span className={styles.counter__count}>{ this.props.count }</span>
+        </p>
+
       </div>
     );
   }
@@ -98,13 +123,18 @@ const Counter = connect(mapCounterStateToProps)(DisconnectedCounter);
 
 export class App extends React.Component {
 
+
   render() {
-    console.log('props', this.props);
+    const clz = styles.wrapper + " " + styles.counters;
+
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.all}>
         <ConnectedContainer {...this.props} />
+        <div className={clz}>
+        <h2>Counters</h2>
         <Counter id="1" increment={this.props.increment} {...this.props.counters["1"]} />
         <Counter id="2" increment={this.props.increment} {...this.props.counters["2"]} />
+        </div>
       </div>
     )
   }
